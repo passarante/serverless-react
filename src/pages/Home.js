45fdde5213232b58c407f5873e35d353abbb428e
Guney Dirim <passarante@gmail.com>
2020-05-30 18:06:38 +0300

    styled home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import CTA from '../styled/CTA';
+import { Accent, StyledTitle } from '../styled/Random';
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <StyledTitle>Ready to type?</StyledTitle>
+      <CTA to="/game">
+        Click or type <Accent>'s'</Accent> to start playing!
+      </CTA>
+    </div>
+  );
 };
 
 export default Home;
